b03a32bebff0856f326659a6604f9f8d9d7cc734
Karabo Phooko <Kmphooko@gmail.com>
2024-09-07 14:43:06 +0200

    updated views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+
 
 # List all books
 class BookListView(generics.ListCreateAPIView):

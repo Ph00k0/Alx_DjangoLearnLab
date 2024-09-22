ea7b0c030d414428cb1fe077ba30675fca15517c
Karabo Phooko <Kmphooko@gmail.com>
2024-09-22 13:19:49 +0200

    updated Isauth

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -5,6 +5,7 @@ from .serializers import PostSerializer, CommentSerializer
 from .permissions import IsAuthorOrReadOnly
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
+from rest_framework.permissions import IsAuthenticated
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
@@ -17,6 +18,14 @@ class PostViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()  # Retrieves all comments
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        # You can customize this if you need to filter comments, e.g., by post
+        return Comment.objects.all()  # Fetches all comments
 
 
 # Create your views here.

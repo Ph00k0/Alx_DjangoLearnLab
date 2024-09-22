c04063c49c371c2252ce739c8d21815e0f81f087
Karabo Phooko <Kmphooko@gmail.com>
2024-09-22 13:41:55 +0200

    update permissions

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
 
 from rest_framework import generics, status
 from rest_framework.response import Response
@@ -13,6 +14,7 @@ from django.shortcuts import get_object_or_404
 from .models import CustomUser
 from rest_framework.permissions import IsAuthenticated
 
+["permissions.IsAuthenticated"]
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
